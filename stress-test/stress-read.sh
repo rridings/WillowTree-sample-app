b15edada95d16024c067f6e7a1fd892e50f00990
Rob Ridings <rob.ridings@gmail.com>
2016-01-18 08:13:40 -0500

    Added usage statement

--- stress-test/stress-read.sh
+++ stress-test/stress-read.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if [  $# -le 0 ] 
+then 
+	echo -e "\nUsage:\n$0 [number of concurrent requests] \n"
+	exit 1
+fi 
+
 /ScholarOne/httpd-2.2.19_64bit_web/bin/ab -n 1 -c 1 -p post -v 4 "http://localhost:4567/asset"
 
 /ScholarOne/httpd-2.2.19_64bit_web/bin/ab -n $1*100 -c $1 -v 1 "http://localhost:4567/asset/myorg://users/test"
--- stress-test/stress-write.sh
+++ stress-test/stress-write.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+if [  $# -le 0 ] 
+then 
+	echo -e "\nUsage:\n$0 [number of concurrent requests] \n"
+	exit 1
+fi 
+
 /ScholarOne/httpd-2.2.19_64bit_web/bin/ab -n $1*100 -c $1 -p post -v 1 "http://localhost:4567/asset"
 
